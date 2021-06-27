87a2eadd866d17d9d39897fdc6ecee516cba0fed
steve zhang <871506263@qq.com>
2021-06-28 00:44:14 +0800

    提交代码于: 2021-06-28 00:44:14

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'; // 比如引入的依赖
 import ReactDOM from 'react-dom'; // 比如引入的依赖
-import './assets/css/index.css'; // 入口文件index样式
 import App from './App'; // 根组件
 
 ReactDOM.render(
