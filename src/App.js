c3ef0b71dbf7e34a7acd9550cd69fb9d2b76d33a
steve zhang <871506263@qq.com>
2021-06-30 00:25:51 +0800

    提交代码于: 2021-06-30 00:25:51

--- src/App.js
+++ src/App.js
@@ -3,30 +3,62 @@ import './assets/app.css';
 
 // Square组件
 class Square extends React.Component {
+  // 所有有 constructor 的 React 组件类应该在其中首先调用 super(props) 函数
   constructor(props) {
     super(props);
 
     this.state = {
-      index: undefined,
+      index: null
     };
   }
+
+  // // handleClick事件
+  // handleClick(index, e) {
+  //   console.log(`e`, e);
+  //   e.preventDefault();
+  //   console.log(`index:`, index);
+  //   this.setState({
+  //     index: index
+  //   });
+  // }
+
   render() {
     console.log(`this.props:`, this.props);
     return (
-      <button className={`square square-${this.props.index}`} onClick={e => alert(this.props.index)}>
-        {this.state.index}
-      </button>
+      // <button className={`square square-${this.props.index}`} onClick={this.handleClick.bind(this, this.props.index)}>
+      //   {this.state.index}
+      // </button>
+      <button className={`square square-${this.props.index}`}>{this.state.index}</button>
     );
   }
 }
 
+// Board组件
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  // handleClick事件
+  handleClick(index, e) {
+    console.log(`Board handleClick e`, e);
+    e.preventDefault();
+    console.log(`Board handleClick index:`, index);
+    // this.setState({
+    //   index: index
+    // });
+  }
+
   renderSquare(index) {
-    return <Square index={index} />;
+    return <Square onClick={this.handleClick.bind(this, index)} index={this.state.squares[index]} />;
   }
 
   render() {
-    const status = 'Next player: 张娜拉';
+    const status = 'Next player: 柯南';
 
     return (
       <div>
@@ -51,6 +83,7 @@ class Board extends React.Component {
   }
 }
 
+// Game组件
 class Game extends React.Component {
   render() {
     return (
