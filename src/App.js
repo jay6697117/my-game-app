1f0fc4c54d93358a654c128a2ea45691c5d81f24
zhangjinhui <871506263@qq.com>
2021-06-30 18:19:21 +0800

    提交代码于: 2021-06-30 18:19:21

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import _ from 'lodash';
 import './assets/app.css';
 
 // Board儿子: Square组件
-class Square extends React.Component {
-  render() {
-    // console.log(`Square this.props:`, this.props);
-    return (
-      <button className={`square square-${this.props.index}`} onClick={e => this.props.clickBoard(e)}>
-        {this.props.index}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   render() {
+//     console.log(`Square this.props:`, this.props);
+//     return (
+//       <button className={`square square-${this.props.index}`} onClick={e => this.props.clickBoard(e)}>
+//         {this.props.index}
+//       </button>
+//     );
+//   }
+// }
+// Board儿子: Square组件
+function Square(props) {
+  console.log(`Square props:`, props);
+  return (
+    <button
+      style={{ color: props.index === 'X' ? 'red' : 'blue', backgroundColor: props.isGameOver ? '#ccc' : '' }}
+      className={`square square-${props.index}`}
+      onClick={props.clickBoard}>
+      {props.index}
+    </button>
+  );
 }
 
 // Game儿子: Board组件: 游戏的 state 统一保存在 Board 组件中而不是各个 Square 组件中
@@ -21,7 +33,9 @@ class Board extends React.Component {
     super(props);
 
     this.state = {
-      squares: Array(9).fill(undefined)
+      squares: Array(9).fill(undefined),
+      isNowX: true,
+      isGameOver: false
     };
   }
 
@@ -30,23 +44,88 @@ class Board extends React.Component {
     // console.log(`Board handleClick this:`, this);
     console.log(`Board handleClick index:`, index);
     // console.log(`BoardhandleClick e:`, e);
+
+    if (calculateWinner(this.state.squares)) {
+      alert('游戏已经结束');
+      return;
+    }
+
     const tempSquares = _.clone(this.state.squares);
-    tempSquares[index] = 'X';
+    tempSquares[index] = this.state.isNowX ? 'X' : 'O';
     this.setState({
-      squares: tempSquares
+      squares: tempSquares,
+      isNowX: !this.state.isNowX
+    });
+
+    this.timer = setTimeout(() => {
+      if (calculateWinner(this.state.squares)) {
+        this.setState({
+          isGameOver: true
+        });
+      }
+    }, 0);
+  }
+
+  componentWillMount() {
+    clearTimeout(this.timer);
+  }
+
+  handleRestart(e) {
+    console.log(`handleRestart e:`, e);
+    console.log(`handleRestart this:`, this);
+
+    let isStart = false;
+    for (let index = 0; index < this.state.squares.length; index++) {
+      const element = this.state.squares[index];
+      if (element !== undefined) {
+        isStart = true;
+        break;
+      }
+    }
+
+    if (!isStart) {
+      alert('游戏还未开始');
+      return;
+    }
+
+    if (!calculateWinner(this.state.squares)) {
+      alert('游戏还在进行中');
+      return;
+    }
+
+    this.setState({
+      squares: Array(9).fill(undefined),
+      isNowX: true
     });
   }
 
   renderSquare(index) {
-    return <Square clickBoard={this.handleClick.bind(this, index)} index={this.state.squares[index]} />;
+    return (
+      <Square
+        isGameOver={this.state.isGameOver}
+        index={this.state.squares[index]}
+        clickBoard={this.handleClick.bind(this, index)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: 柯南';
+    let status;
+    const winner = calculateWinner(this.state.squares);
+
+    if (winner) {
+      status = `Winner is: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.isNowX ? 'X' : 'O'}`;
+    }
 
     return (
       <div>
         <div className='status'>{status}</div>
+        <button className='btn' onClick={this.handleRestart.bind(this)}>
+          重新开始
+        </button>
+        <div style={{ backgroundColor: this.state.isGameOver ? '#ccc' : '' }}></div>
         <div className='board-row'>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -82,6 +161,28 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+// 判断是否胜利并结束
+function calculateWinner(squares) {
+  console.log(`calculateWinner squares`, squares);
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return undefined;
 }
 
 export default Game;
